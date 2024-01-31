896fa7ac52c14d240667d48024d86266515d5b33
moududulhaque <moududulhaque@gmail.com>
2024-01-31 19:39:20 +0600

    This is Object Practices

--- js-object/object-tasks.js
+++ js-object/object-tasks.js
@@ -1,81 +1,73 @@
 // ### Task-1
 // Access the `golden rod` color value in output.
 // ```
-// const colors = {
-//     red: "#ff0000",
-//     green: "#00ff00",
-//     blue: "#0000ff",
-//     "golden rod": '#daa520'
-// };
-// ```
-// ---
+const colors = {
+    red: "#ff0000",
+    green: "#00ff00",
+    blue: "#0000ff",
+    "golden rod": '#daa520'
+};
+console.log(colors["golden rod"]);
+
 // ### Task-2
 // For this object below add a property named `passenger capacity` with value 5
-// ```js
-// const car = {
-//     make: "Toyota",
-//     model: "Corolla",
-//     year: 2020
-// };
+const car = {
+    make: "Toyota",
+    model: "Corolla",
+    year: 2020,
+};
+car["passenger capacity"] = 5
+console.log(car);
 
-// ```
-// ---
+
 // ### Task-3
 // Display the physics marks as output.
-// ```js
-// const student = {
-//     name: "Hamim Sakep",
-//     id: 5421,
-//     physics: {
-//         subject: "HSC Physics",
-//         author: "Shahjahan Tapan",
-//         marks: 30
-//     }
-// };
-// ```
-
-// ---
+const student = {
+    name: "Hamim Sakep",
+    id: 5421,
+    physics: {
+        subject: "HSC Physics",
+        author: "Shahjahan Tapan",
+        marks: 30
+    }
+}
+console.log(student.physics.marks);
 
 // ### Task-4
-
 // Count the `number of properties`.
+let student2 = {
+    name: 'Ariana Grande',
+    age: 21,
+    city: 'Gaibandha',
+    n: 'dick',
+    isStudent: true
+};
+// const totalProp=Object.keys(student2.length);
+// console.log(totalProp);
+for (const prop in student2) {
+    console.log(prop);
+    // console.log(student2[prop]);
+}
 
-// **Input:**
-// <br>
-// ```js
-// let student = {
-//     name: 'Ariana Grande',
-//     age: 21,
-//     city: 'Gaibandha',
-//     isStudent: true
-// };
-// ```
-// **Output:**
 
-// <br>
-
-//     4
-
-// ---
 // ### Task-5 (Hard)
-
 // Loop through an object and print the key-value pairs with their `types`
+let myObject = {
+    name: 'Monyem',
+    age: 25,
+    city: 'Rangpur',
+    isStudent: true
+};
 
-// **Input:**
-// <br>
+for (const key in myObject){
+    const value = myObject[key];
+    const type = typeof value;
 
-// let myObject = {
+    console.log(`${key}: ${value} (Type: ${type})`);
+}
 
-//     name: 'John Doe',
-//     age: 25,
-//     city: 'Example City',
-//     isStudent: true
 
-// };
 
-// **Output:**
-
-// <br>
 
 //     key: name | type:  string
 //     key: age | type:  number
--- js-string/string-tasks.js
+++ js-string/string-tasks.js
@@ -1,27 +1,23 @@
 // ### Task-1: 
 // Count how many times a string has the letter `a`
-let land = ['Bangladesh'];
-console.log(land);
-
-const country = 'Bangladesh';
-console.log(country[2]);
+const country = 'Location of Bangladesh';
+console.log(country.split('a').length - 1);
 
 // ---
 // ### Task-2: 
 // Count how many times a string has the letter `a` or `A`
-
 // ---
 
 // ### Task-3: 
-
 // Check whether a string contains all the vowels `a`, `e`, `i`, `o`, `u` 
-
 // ---
 
 // ### Task-4: 
 // If a given string has either x, replace x by y. if the given string has X, replace it by Y.
-
 //     Hint: You should be able to check whether x or X exists. After that, search online how to replace a character in a string.
+const letter = 'x';
+
+
 
 // ---
 // ### Task-5:
