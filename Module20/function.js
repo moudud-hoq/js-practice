de34d11f9d1d470fbcef207c11e379b10528c50f
moududulhaque <moududulhaque@gmail.com>
2024-02-02 22:04:26 +0600

    07-------------------------Baba Mayer Age er differences

--- Module20/function.js
+++ Module20/function.js
@@ -1,5 +1,5 @@
 
-//1----------Function Basic Structure
+//1----------------------------------------Function Basic Structure
 function ektaMeaningfulName() {
     console.log('I love Allah')
     console.log('I love Rasul SallahuAlaihi Wa Sallam')
@@ -7,14 +7,14 @@ function ektaMeaningfulName() {
 //Call the function
 ektaMeaningfulName();
 
-//2-----------Square/Borgo of any number
+//2---------------------------------------Square/Borgo of any number
 function square(number) {
     console.log(square = number * number);
 }
 //call the function
 square(105);
 
-//3-----------Function of Multiple numbers
+//3-------------------------------------Function of Multiple numbers
 function multipleSquare(num1, num2, num3,) {
     const sum = num1 + num2 + num3;
     const divi = num1 - num2 - num3;
@@ -27,10 +27,45 @@ function multipleSquare(num1, num2, num3,) {
 multipleSquare(3, 5, 8);
 
 
-//Checking individual numbers of student
-function addAll (Math, Biology, Bengali, Chemistry, Physics){
-    const total= Math+ Biology+ Bengali+ Chemistry+ Physics;
+//04 ---------------------------------Checking individual numbers of student
+function addAll(Math, Biology, Bengali, Chemistry, Physics) {
+    const total = Math + Biology + Bengali + Chemistry + Physics;
     console.log('Checking individual numbers of student :', Math, Biology, Bengali, Chemistry, Physics);
     console.log('Result of student :', total);
 }
-addAll(50,60,70,80,90);
+addAll(50, 60, 70, 80, 90);
+
+//05---------------------------------------------Recapping Function
+function doubleCount(number) {
+    const doubled = number * 3;
+    console.log(number, doubled);
+}
+console.log('Calling the function:');
+doubleCount(11);
+
+//If we want to call a variable
+const firstDeclaringVarnameInAFunction = 5;
+doubleCount(firstDeclaringVarnameInAFunction);
+
+//06------------------------------------------Recapping Function
+function singleCount(number) {
+    const singel = number;
+    console.log(number, singel);
+}
+//Simple single count in a market
+console.log('Calling the Simple single count in a market:');
+let myMoney = 10;
+let apple = 2;
+let orange = 3;
+let totalCost = 2 + 3;
+let ShopkeeperWillReturn = myMoney - totalCost;
+singleCount(ShopkeeperWillReturn);
+
+//07-------------------------Baba Mayer Age er differences
+function differencesBtwAgeOfBabaMa(babarAge, mayerAge) {
+    const diff = babarAge - mayerAge;
+    console.log(babarAge, mayerAge, 'difference', diff);
+}
+const fatherAge = 60;
+const motherAge = 45;
+differencesBtwAgeOfBabaMa(fatherAge, motherAge);
