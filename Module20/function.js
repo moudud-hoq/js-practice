87672ef758c386eb509d8a1a343c0a1113396641
moududulhaque <moududulhaque@gmail.com>
2024-02-02 20:29:54 +0600

    Function of Multiple numbers

--- Module20/function.js
+++ Module20/function.js
@@ -1,5 +1,5 @@
 
-//Function Basic Structure
+//1----------Function Basic Structure
 function ektaMeaningfulName() {
     console.log('I love Allah')
     console.log('I love Rasul SallahuAlaihi Wa Sallam')
@@ -7,9 +7,21 @@ function ektaMeaningfulName() {
 //Call the function
 ektaMeaningfulName();
 
-//Square/Borgo of any number
+//2-----------Square/Borgo of any number
 function square(number) {
     console.log(square = number * number);
 }
 //call the function
 square(105);
+
+//3-----------Function of Multiple numbers
+function multipleSquare(num1, num2, num3,) {
+    const sum = num1 + num2 + num3;
+    const divi = num1 - num2 - num3;
+    const multiply = num1 * num2 * num3;
+    console.log(sum);
+    console.log(divi);
+    console.log(multiply);
+}
+//Call
+multipleSquare(3, 5, 8);
