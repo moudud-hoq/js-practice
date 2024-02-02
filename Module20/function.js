3f12be62a9fc88ec370b907779c91b428eb3bcbe
moududulhaque <moududulhaque@gmail.com>
2024-02-02 20:19:07 +0600

    Function square & Call the function

--- Module20/function.js
+++ Module20/function.js
@@ -1,8 +1,15 @@
 
 //Function Basic Structure
-function ektaMeaningfulName (){
+function ektaMeaningfulName() {
     console.log('I love Allah')
     console.log('I love Rasul SallahuAlaihi Wa Sallam')
 }
 //Call the function
 ektaMeaningfulName();
+
+//Square/Borgo of any number
+function square(number) {
+    console.log(square = number * number);
+}
+//call the function
+square(105);
