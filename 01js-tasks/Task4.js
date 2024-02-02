2b6ca9f9624d13ac64560e724640837939701965
moududulhaque <moududulhaque@gmail.com>
2024-02-02 19:56:45 +0600

    Function Basic Structure & Call the function

--- 01js-tasks/Task4.js
+++ 01js-tasks/Task4.js
@@ -5,5 +5,5 @@
 // Explain your answers.
 
 var a = isNaN('11');
-// var a=isNaN(2-10);
+var a=isNaN(2-10);
 console.log(a);
---
+++ Module20/function.js
@@ -0,0 +1,8 @@
+
+//Function Basic Structure
+function ektaMeaningfulName (){
+    console.log('I love Allah')
+    console.log('I love Rasul SallahuAlaihi Wa Sallam')
+}
+//Call the function
+ektaMeaningfulName();
