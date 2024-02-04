9ba00b2da969f67975c8f6df3ca8772544642c50
moududulhaque <moududulhaque@gmail.com>
2024-02-04 15:27:43 +0600

    Simple if Else use and Ternary Operator

--- 17conditional/17-7ternary.js
+++ 17conditional/17-7ternary.js
@@ -11,8 +11,8 @@ console.log(price);
 
 //shortcode of above code
 /* No Need for now, when I'll be the good person in JS the it will apply
-let price2 = 1000;
+*/let price2 = 1000;
 const isLeader2 = true;
 price2 = isLeader2 === false ? 0 : price2 + 100;
 console.log(price2);
-*/
+
--- 17conditional/5ternary-number.js
+++ 17conditional/5ternary-number.js
@@ -8,5 +8,23 @@ if num1 is bigger than num2 then result will be double of num1. if not, then the
 write a simple if-else. 
 
 also, write it using ternary operator.
-
  */
+let num1 = 25;
+let num2 = 30;
+let result;
+
+if (num1 > num2) {
+    result = num1 * 2;
+} else {
+    result = num1 + num2;
+}
+console.log(result);
+// Ternary of the above code
+let num3 = 5;
+let num4 = 3;
+let result2 = (num3 > num4) ? num3 * 2 : num3 + num4;
+console.log(result2)
+
+
+
+
