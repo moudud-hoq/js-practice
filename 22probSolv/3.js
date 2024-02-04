4a7ff7e8a9b9f29524a9b5aca7335d9ae701b036
moududulhaque <moududulhaque@gmail.com>
2024-02-04 19:44:44 +0600

    Price of your total buying cloth

--- 22probSolv/3.js
+++ 22probSolv/3.js
@@ -12,5 +12,23 @@ function quantityOfWood(chairQuantity, tableQuantity, bedQuantity) {
 
     return totalWood;
 }
-const wood = quantityOfWood(0,1,2);
-console.log("You will need the wood to make some furnitures :", wood);
+const wood = quantityOfWood(0, 1, 2);
+console.log("You will need the wood to make some furnitures :", wood)
+
+
+//Same Task 2
+function quantityOfCloth(shirt, pant, shoe) {
+    const shirtPrice = 500;
+    const pantPrice = 300;
+    const shoePrice = 200;
+
+    const totalPant = pant * pantPrice;
+    const totalShirt = shirt * shirtPrice;
+    const totalShoe = shoe * shoePrice;
+
+    const totalCLoth = totalPant + totalShirt + totalShoe;
+    return totalCLoth;
+}
+
+const cloth = quantityOfCloth(0, 0, 2);
+console.log("Price of your total buying cloth :", cloth)
