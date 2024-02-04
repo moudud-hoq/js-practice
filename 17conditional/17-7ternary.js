1ed8ab821030882e7f7e99a85a573432f0bd32b4
moududulhaque <moududulhaque@gmail.com>
2024-02-04 09:27:59 +0600

    Boolean COnditionals

---
+++ 17conditional/17-7ternary.js
@@ -0,0 +1,10 @@
+//if else shorthand by ternary operator
+let price = 1000;
+const isLeader = false;
+if (isLeader === false) {
+    price = 0;
+} else (
+    price = price + 100
+)
+console.log(price);
+
