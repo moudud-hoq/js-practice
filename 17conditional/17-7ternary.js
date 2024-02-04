864995595dfd8fc6f48d471221c6843a563cb2db
moududulhaque <moududulhaque@gmail.com>
2024-02-04 09:44:29 +0600

    Ternary Learn

--- 17conditional/17-7ternary.js
+++ 17conditional/17-7ternary.js
@@ -1,6 +1,7 @@
 //if else shorthand by ternary operator
 let price = 1000;
 const isLeader = false;
+
 if (isLeader === false) {
     price = 0;
 } else (
@@ -8,3 +9,10 @@ if (isLeader === false) {
 )
 console.log(price);
 
+//shortcode of above code
+/* No Need for now, when I'll be the good person in JS the it will apply
+let price2 = 1000;
+const isLeader2 = true;
+price2 = isLeader2 === false ? 0 : price2 + 100;
+console.log(price2);
+*/
