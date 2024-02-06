a76565fd654a6aaa865217daf63e25c08a4f8929
moududulhaque <moududulhaque@gmail.com>
2024-02-06 08:59:43 +0600

    Match Finder

--- batch8/match-finder.js
+++ batch8/match-finder.js
@@ -1,33 +1,19 @@
+//You will got a function name matchFinder()
+//Take 2 string as input
+//If get similarities between 1st side of the string and full of last side of the string
+//Then return boolean data types, true
+//If not get similarities then return, false
+//Bonus: Both input are string or not check. If both or one not string then return a meaningfull message
+
 function matchFinder(string1, string2) {
-
-    //we will try to find match between the two strings
-    const result = string1.includes(string2)
-
-
-    //return true or false according to the match
-    return result;
-}
-console.log(matchFinder('John Doe', 'ohn'));
-console.log(matchFinder('Javascript', 'Code'));
-
-
-
-
-//For bonus
-function matchFinder(string1, string2) {
-
-    //Show Error Message
+    //Error message
+    //যদি string1 একটা type of string না হয় এবং যদি string2 একটা type of string না হয় তাহলে মেসেজ হিসেবে return করতে হবে একটা অর্থবহ বার্তা
     if (typeof string1 !== 'string' || typeof string2 !== 'string') {
-        return "Please provide a string..."
+        return "Baba input a valid string otherwise it will not work"
     }
 
-
-    //we will try to find match between the two strings
-    const result = string1.includes(string1)
-
-
-    //return true or false according to the match
-    return result;
+    const similarities = string1.includes(string2);
+    return similarities;
 }
-console.log(matchFinder('Peter Parker', 'Pen'));
+console.log(matchFinder('Peter Parker', 2));
 console.log(matchFinder('Peter Parker', 'pet'));
