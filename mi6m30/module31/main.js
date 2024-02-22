7ef5f63c62ed6ad83bab5df16b4e6827cf4214ee
moududulhaque <moududulhaque@gmail.com>
2024-02-22 20:24:46 +0600

    Access Value, Nested Object, Optional Chaining

--- mi6m30/module31/main.js
+++ mi6m30/module31/main.js
@@ -1,4 +1,31 @@
 //31-2 Access Value, Nested Object, Optional Chaining
 //data access
 const data = [{ id: 1, name: "abul", address: "Dhaka" }];
-console.log(data[0].name)
+console.log(data[0].name);
+
+const products = {
+  count: 500,
+  data2: [
+    { id: 1, name: "Moudud", price: 5000 },
+    { id: 2, name: "Moudud2", price: 8000 },
+  ],
+};
+
+console.log(products.data2[1].price);
+
+const person = {
+  id: 1,
+  name: "Tania",
+  address: {
+    present: "Dhaka",
+    permanent: "Rangpur",
+    street: {
+      first: "54/A",
+      second: "KHA",
+      third: "Rangpur",
+    },
+  },
+};
+
+console.log(person.address.street.first);
+console.log(person.address.street.third);
