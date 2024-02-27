f3733dcd8ff6a4c17b20cb732b6512fa1359d801
moududulhaque <moududulhaque@gmail.com>
2024-02-27 09:54:27 +0600

    A few knodledge gather about GET method

--- 33API/js/fetch.js
+++ 33API/js/fetch.js
@@ -12,3 +12,53 @@ function displayPhotoData(photoData) {
     ul.appendChild(li);
   }
 }
+
+//post data
+function postData() {
+  fetch("https://jsonplaceholder.typicode.com/posts")
+    .then((res) => res.json())
+    .then((postData) => displayPostData(postData));
+}
+
+function displayPostData(posts) {
+  const postContainer = document.getElementById("post-container");
+  //1. get the container element where you want to add the new elements
+  //2. Create CHild element
+  //3. Set innerText or innerHTML
+  //4. Do appendChild the innerText or innerHTML
+  for (const post of posts) {
+    const postDiv = document.createElement("div");
+    postDiv.classList.add("post");
+    postDiv.innerHTML = `
+    <h3>Post Id: ${post.id}</h3>
+    <h4>Post Title: ${post.title}</h4>
+    <p>Post Description:${post.body}</p>
+    `;
+    postContainer.appendChild(postDiv);
+  }
+}
+postData();
+
+// SHow the user list
+function userDataList() {
+  fetch("https://jsonplaceholder.typicode.com/users")
+    .then((res) => res.json())
+    .then((userDataList) => displayUserData(userDataList));
+}
+
+function displayUserData(users) {
+  const userDataContainer = document.getElementById("user-data-container");
+
+  for (const user of users) {
+    const usersDiv = document.createElement("div");
+    usersDiv.classList.add("user");
+    usersDiv.innerHTML = `
+    <h3>User-${user.id}</h3>
+    <h4>Name: ${user.name}</h4>
+    <p>Email:${user.email}</p>
+    <p>Address:${user.address.city}</p>
+    `; //    <p>Address: ${JSON.stringify(user.address)}</p>
+    userDataContainer.appendChild(usersDiv);
+  }
+}
+userDataList();
