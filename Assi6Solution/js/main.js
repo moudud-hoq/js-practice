a004b1b5dce2ebacb095d96068487545a400fac3
moududulhaque <moududulhaque@gmail.com>
2024-02-29 07:31:25 +0600

    Step 1 and Step 2 Done, and step 2.1 is after step 2 writing, because, there we added style of Tailwind

--- Assi6Solution/js/main.js
+++ Assi6Solution/js/main.js
@@ -1,2 +1,19 @@
-console.log('Angta lagse?')
-console.log('Ha lagse?')
+//Step 1:
+const btnContainer = document.getElementById("btn-container");
+
+//step 2: copy the api
+const fetchCategories = () => {
+  const url = "https://openapi.programming-hero.com/api/videos/categories";
+  fetch(url)
+    .then((res) => res.json())
+    .then(({ data }) => {
+      data.forEach((card) => {
+        const newBtn = document.createElement("button");
+        newBtn.className = "btn  btn-ghost bg-slate-700 text-white text-lg";//2.1
+        newBtn.innerText = card.category;
+        btnContainer.appendChild(newBtn);
+      });
+    });
+};
+
+fetchCategories();
